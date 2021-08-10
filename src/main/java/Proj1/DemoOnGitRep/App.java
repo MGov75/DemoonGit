2a32cf816cce8bc961c748d61036f66f2f30a1e7
Arpana <Arpana@Lenovo>
2021-08-10 23:29:38 +0530

    First Commit

---
+++ src/main/java/Proj1/DemoOnGitRep/App.java
@@ -0,0 +1,13 @@
+package Proj1.DemoOnGitRep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Proj1/DemoOnGitRep/TestCase1.java
@@ -0,0 +1,10 @@
+package Proj1.DemoOnGitRep;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("First Test Case");
+	}
+
+}
